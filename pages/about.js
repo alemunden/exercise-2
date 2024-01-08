24a77277fe283f41ca6f2ddf1455d257b061c3fa
ale munden <amunden@my.bcit.ca>
2024-01-08 12:09:20 -0800

    new files and folders

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Header from "@/components/Header"
+import Footer from "@/components/footer"
+
+export default function About() {
+    return(
+        <>
+            <Header/>
+            <main className={styles.main}>
+
+            </main>
+            <Footer/>
+        </>
+    )
+}
